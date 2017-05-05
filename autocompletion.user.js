9acbc77adaaa801201ae52d11f19e9878f5e4572
JimRaynor <JimRaynor@gmail.com>
2017-05-05 22:53:09 +1000

    refactor: use select2 for better UX

--- autocompletion.user.js
+++ autocompletion.user.js
@@ -3,49 +3,28 @@
 // Copyright (c) Jim Zhai
 //-----------------------------------------------------------------------------
 // ==UserScript==
-// @name			Wod auto completion
-// @namespace		org.toj
-// @version			0.1
-// @description		Add auto completion text field for item type and skill bonus selector
-// @include     	http*://*.world-of-dungeons.*/wod/spiel/hero/items.php*
-// @include     	http*://*.world-of-dungeons.*/wod/spiel/trade/trade.php*
-// @require			https://raw.githubusercontent.com/LeaVerou/awesomplete/gh-pages/awesomplete.min.js
-// @resource    	https://raw.githubusercontent.com/LeaVerou/awesomplete/gh-pages/awesomplete.css
+// @name          Wod auto completion
+// @namespace     org.toj
+// @version       0.2
+// @description   Add auto completion text field for item type and skill bonus selector
+// @include       http*://*.world-of-dungeons.*/wod/spiel/hero/items.php*
+// @include       http*://*.world-of-dungeons.*/wod/spiel/trade/trade.php*
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @require       https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js
+// @resource      select2-css https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css
+// @grant         GM_addStyle
+// @grant         GM_getResourceText
 // ==/UserScript==
-(function() {
-  var fieldPattern = [/^item_\d+item_class$/i, /^item_\d+any_skill$/i, /^item_\d+any_skillclass$/i, /^item_\d+skill$/i];
-  var selects = document.getElementsByTagName("select");
-  for (var i = 0; i < selects.length; i++) {
-    select = selects[i];
-    var name = select.getAttribute("name");
-    var needsAutoCompletion = false;
-    fieldPattern.forEach(function(pattern) {
-      if (name.search(pattern) != -1) {
-        needsAutoCompletion = true;
-      }
+
+GM_addStyle(GM_getResourceText("select2-css"));
+GM_addStyle('.select2-dropdown {color: #FFFFFF !important; background-color: #665B47 !important; border-color: #816E36 !important;}');
+GM_addStyle('.select2-container--default .select2-selection--single {color: #FFFFFF !important; background-color: #665B47 !important; border-color: #816E36 !important;}');
+GM_addStyle('.select2-container--default .select2-selection--single .select2-selection__rendered {color: #FFFFFF !important; background-color: #665B47 !important; border-color: #816E36 !important;}');
+
+(function ($, undefined) {
+  $(function () {
+    $("select").select2({
+        minimumResultsForSearch: 7
     });
-    if (!needsAutoCompletion) {
-      continue;
-    }
-    var itemAutoCompletion = document.createElement("input");
-    itemAutoCompletion.id = name + "-auto-completion";
-    itemAutoCompletion.setAttribute("for", name);
-    select.parentElement.appendChild(itemAutoCompletion);
-
-    var itemList = [];
-    var options = select.children;
-    for (var j = 0; j < options.length; j++) {
-      itemList[j] = {label: options[j].text, value: options[j].getAttribute("value")};
-    }
-    new Awesomplete(itemAutoCompletion, {minChars: 1, maxItems: 255, list: itemList});
-  }
-
-  addEventListener("awesomplete-select", function(e) {
-    var select = document.getElementsByName(e.srcElement.getAttribute("for"))[0];
-    select.value = e.text.value;
-  }, false);
-
-  addEventListener("awesomplete-selectcomplete", function(e){
-    e.srcElement.value = e.text.label;
-  }, false);
-})();
+  });
+})(window.jQuery.noConflict(true));
