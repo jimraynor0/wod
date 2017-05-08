62cecfc242a5c5cef925be41f7c3569580c7b5a2
Jim Zhai <jim.zhai@skiddoo.com.au>
2017-05-08 13:22:26 +1000

    wip

--- autocompletion.user.js
+++ autocompletion.user.js
@@ -4,8 +4,9 @@
 //-----------------------------------------------------------------------------
 // ==UserScript==
 // @name          Wod auto completion
+// @author        Jim Zhai
 // @namespace     org.toj
-// @version       0.3
+// @version       0.4
 // @description   Add auto completion text field for item type and skill bonus selector
 // @include       http*://*.world-of-dungeons.*/wod/spiel/hero/items.php*
 // @include       http*://*.world-of-dungeons.*/wod/spiel/trade/trade.php*
@@ -16,22 +17,55 @@
 // ==/UserScript==
 
 GM_addStyle(GM_getResourceText("select2-css"));
-GM_addStyle('.select2-dropdown {color: #FFFFFF !important; background-color: #665B47 !important; border-color: #816E36 !important;}');
-GM_addStyle('.select2-container--default .select2-selection--single {color: #FFFFFF !important; background-color: #665B47 !important; border-color: #816E36 !important;}');
-GM_addStyle('.select2-container--default .select2-selection--single .select2-selection__rendered {color: #FFFFFF !important; background-color: #665B47 !important; border-color: #816E36 !important;}');
+GM_addStyle('.select2-results {max-height: 70vh; !important}');
+GM_addStyle('.select2-container--default .select2-results>.select2-results__options {max-height: 70vh; !important}');
 
 var minOptionCount = 12;
 var ignoreFields = [/^EquipItem\[\d+\]$/i, /^sndgrp\d*\[\d+\]$/i];
 
 if(typeof unsafeWindow.jQuery !== "undefined") {
-  var $ = jQuery = unsafeWindow.jQuery;
+  var $ = unsafeWindow.jQuery;
+
+  var height;
+  var backgroundColor;
+  var fontColor;
+  var borderColor;
+  var fontSize;
 
   $("select").each(function() {
     $this = $(this);
     if (minOptionCount > 0 && $this.find("option").length > minOptionCount && doNotIgnore($this.prop("name"))) {
       $this.select2();
+      height = $this.css("height");
+      backgroundColor = $this.css("background-color");
+      fontColor = $this.css("color");
+      borderColor = $this.css("border-color");
+      fontSize = $this.css("font-size");
     }
   });
+
+  $(".select2-selection--single, .select2-selection__rendered").css("height", height);
+  $(".select2-selection--single, .select2-selection__rendered").css("font-size", fontSize);
+
+  $(".select2-container--default .select2-selection--single .select2-selection__arrow").css("height", height);
+  $(".select2-container--default .select2-selection--single .select2-selection__arrow").css("top", height / 2);
+
+  $(".select2-dropdown").css("background-color", backgroundColor);
+  $(".select2-dropdown").css("color", fontColor);
+  $(".select2-dropdown").css("border-color", borderColor);
+  $(".select2-dropdown").css("font-size", fontSize);
+
+  $(".select2-container--default .select2-selection--single").css("background-color", backgroundColor);
+  $(".select2-container--default .select2-selection--single").css("color", fontColor);
+  $(".select2-container--default .select2-selection--single").css("border-color", borderColor);
+  $(".select2-container--default .select2-selection--single").css("line-height", 1);
+  $(".select2-container--default .select2-selection--single").css("font-size", fontSize);
+
+  $(".select2-container--default .select2-selection--single .select2-selection__rendered").css("background-color", backgroundColor);
+  $(".select2-container--default .select2-selection--single .select2-selection__rendered").css("color", fontColor);
+  $(".select2-container--default .select2-selection--single .select2-selection__rendered").css("border-color", borderColor);
+  $(".select2-container--default .select2-selection--single .select2-selection__rendered").css("line-height", 1);
+  $(".select2-container--default .select2-selection--single .select2-selection__rendered").css("font-size", fontSize);
 
   function doNotIgnore(field) {
     if (ignoreFields.length <= 0) {
